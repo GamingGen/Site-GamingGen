e13a31596250a2b739687ea443639c05bbb5ac4c
darkterra <darkterra01@gmail.com>
2016-03-26 18:33:09 +0000

    Last Update Of GamingGen Site build with StrongLoop

--- client/js/app.js
+++ client/js/app.js
@@ -1,69 +1,29 @@
-angular
-  .module('app', [
-    'ui.router',
-    'lbServices'
-  ])
-  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
-      $urlRouterProvider) {
-    $stateProvider
-      .state('add-review', {
-        url: '/add-review',
-        templateUrl: 'views/review-form.html',
-        controller: 'AddReviewController',
-        authenticate: true
-      })
-      .state('all-reviews', {
-        url: '/all-reviews',
-        templateUrl: 'views/all-reviews.html',
-        controller: 'AllReviewsController'
-      })
-      .state('edit-review', {
-        url: '/edit-review/:id',
-        templateUrl: 'views/review-form.html',
-        controller: 'EditReviewController',
-        authenticate: true
-      })
-      .state('delete-review', {
-        url: '/delete-review/:id',
-        controller: 'DeleteReviewController',
-        authenticate: true
-      })
-      .state('forbidden', {
-        url: '/forbidden',
-        templateUrl: 'views/forbidden.html',
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'views/login.html',
-        controller: 'AuthLoginController'
-      })
-      .state('logout', {
-        url: '/logout',
-        controller: 'AuthLogoutController'
-      })
-      .state('my-reviews', {
-        url: '/my-reviews',
-        templateUrl: 'views/my-reviews.html',
-        controller: 'MyReviewsController',
-        authenticate: true
-      })
-      .state('sign-up', {
-        url: '/sign-up',
-        templateUrl: 'views/sign-up-form.html',
-        controller: 'SignUpController',
-      })
-      .state('sign-up-success', {
-        url: '/sign-up/success',
-        templateUrl: 'views/sign-up-success.html'
-      });
-    $urlRouterProvider.otherwise('all-reviews');
-  }])
-  .run(['$rootScope', '$state', function($rootScope, $state) {
-    $rootScope.$on('$stateChangeStart', function(event, next) {
-      // redirect to login page if not logged in
-      if (next.authenticate && !$rootScope.currentUser) {
-        event.preventDefault(); //prevent current page from loading
-        $state.go('forbidden');
-      }
-    });
-  }]);
+(function() {
+  var app = angular.module('store', ['lbServices']);
+  
+  app.controller('StoreController', function(User){
+    this.products = gems;
+  });
+  
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: '1. 2. 3.',
+      canPurchase: true
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: '. . .',
+      canPurchase: false
+    }
+  ];
+  
+})();
+
+var tests = [];
+
+for(var test of tests){
+  console.log(test);
+}
