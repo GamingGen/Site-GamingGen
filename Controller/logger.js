30968e0641af7bd24239f6694c41212042fa45e7
darkterra <darkterra01@gmail.com>
2016-07-02 20:00:38 +0000

    Correction Dossier manquant pour les Logs (gestion de la création du dossier Logs)

--- Controller/logger.js
+++ Controller/logger.js
@@ -1,11 +1,30 @@
 const winston = require('winston');
 winston.emitErrs = true;
 
+const fs = require('fs');
+
+const logPath = './Logs/all-logs.log';
+
+fs.access(logPath, fs.R_OK | fs.W_OK, (err) => {
+  console.log(err ? 'no access!' : 'can read/write');
+  if (err) {
+    fs.mkdir('.Logs', (err) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('Log Folder Created');
+      }
+    });
+  }
+});
+
+
 var logger = new winston.Logger({
   transports: [
     new winston.transports.File({
       level: 'info',
-      filename: './Logs/all-logs.log',
+      filename: logPath,
       handleExceptions: true,
       json: true,
       maxsize: 5242880, //5MB
