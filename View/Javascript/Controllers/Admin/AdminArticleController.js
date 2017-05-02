20556d763212f9c99c18e1b6c814329c109a1f2b
darkterra <darkterra01@gmail.com>
2017-05-02 11:59:47 +0000

    Correction merge auto

--- View/Javascript/Controllers/Admin/AdminArticleController.js
+++ View/Javascript/Controllers/Admin/AdminArticleController.js
@@ -38,9 +38,9 @@ AppControllers.controller('adminArticleCtrl', ['$scope', '$http', 'socket', 'Use
     name   : 'hot_news'
   };
   
-  $http.get('/articles').success(function(articles) {
-    articleCtrl.lstArticles = articles;
-  }).error(function() {
+  $http.get('/articles').then(function(articles) {
+    articleCtrl.lstArticles = articles.data;
+  }).catch(function() {
     $("#msgError").html("Erreur lors de la récupération des articles, veuillez réessayer ultérieurement.");
     $("#msgError").show().delay(3000).fadeOut();
   });
