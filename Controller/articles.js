9233145bb02770f25c5dab0f53554fcfd4f621ba
darkterra <darkterra01@gmail.com>
2017-05-03 10:53:49 +0000

    Correction

--- Controller/articles.js
+++ Controller/articles.js
@@ -73,6 +73,7 @@ router.get('/:id', function (req, res) {
 // -------------------------------------------------------------------------- //
 let articleEvent = function(ServerEvent) {
   ServerEvent.on('saveArticle', function(data, socket) {
+    console.log(socket.request.session.passport);
     if (socket.request.session && socket.request.session.passport && socket.request.session.passport.user && socket.request.session.passport.user.roles && socket.request.session.passport.user.roles.includes('Rédacteur')) {
       console.log(socket.request.session.passport.user);
       var newArticle = new articleSchema({
@@ -103,6 +104,7 @@ let articleEvent = function(ServerEvent) {
     }
   });
   ServerEvent.on('updateArticle', function(data, socket) {
+    console.log(socket.request.session.passport);
     if (socket.request.session && socket.request.session.passport && socket.request.session.passport.user && socket.request.session.passport.user.roles && socket.request.session.passport.user.roles.includes('Rédacteur')) {
       console.log(socket.request.session.passport.user);
       articleSchema.findOneAndUpdate({_id: data._id}, data, {new: true}, function (err, rowUpdated) {
@@ -126,6 +128,7 @@ let articleEvent = function(ServerEvent) {
     }
   });
   ServerEvent.on('rmArticle', function(data, socket) {
+    console.log(socket.request.session.passport);
     if (socket.request.session && socket.request.session.passport && socket.request.session.passport.user && socket.request.session.passport.user.roles && socket.request.session.passport.user.roles.includes('AdminRédacteur')) {
       console.log(socket.request.session.passport.user);
       articleSchema.findOneAndRemove({_id : data._id}, function (err, result) {
