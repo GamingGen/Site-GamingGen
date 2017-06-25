c45c3276e15d8f40dbbcd8940ef04464ad2d0af7
darkterra <darkterra01@gmail.com>
2017-06-25 14:57:34 +0000

    Modification de la localisation de la vidéo

--- View/Javascript/Controllers/FestivalController.js
+++ View/Javascript/Controllers/FestivalController.js
@@ -1,10 +1,17 @@
 'use strict';
 
 var AppControllers = angular.module('AppControllers');
-AppControllers.controller('competitionCtrl', ['$http', '$scope', 'socket', '$filter', function($http, $scope, socket){
+AppControllers.controller('festivalCtrl', ['$http', '$scope', 'socket', '$filter', function($http, $scope, socket){
   // ----- Init -----
   
+  
   // ----- GET / SET Data -----
+  $scope.videoFestival = "1UDR9KEGkTM";
+  $scope.playerVars = {
+    controls: 1,
+    autoplay: 0
+  };
+  
   
   // ----- Public Méthode -----
   
