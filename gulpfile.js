af7ea5032b57f6b1edd2eb8d7cf85422f542f335
darkterra <darkterra01@gmail.com>
2017-09-02 17:07:43 +0000

    Nettoyage + Ajout compression HTML et Images

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@
 const gulp        = require('gulp');
 const jsdoc       = require('gulp-jsdoc3');
 const gulpif      = require('gulp-if');
+const htmlmin     = require('gulp-htmlmin');
 const uglify      = require('gulp-uglify');
-const inifyCss    = require('gulp-clean-css');
+const minifyCss   = require('gulp-clean-css');
 const sass        = require('gulp-sass');
+const image       = require('gulp-image');
 const runSequence = require('run-sequence');
 
 // Include plugins
@@ -15,10 +17,20 @@ const plugins = require('gulp-load-plugins')(); // tous les plugins de package.j
 // Variables de chemins
 const sources     = ['server.js', 'Controller/*.js', 'Model/*.js', 'View/Javascript/Controllers/*.js', 'View/Javascript/Factories/*.js', 'View/Javascript/Tests/*.js'];
 const sassFiles   = ['View/Style/*.+(scss|sass)'];
-const htmlSource  = 'View/index.html';
-const destination = 'View/dist'; // dossier à livrer
+const imageSource = 'View/Img/**';
+const indexSource = 'View/index.html';
+const htmlSource  = 'View/Partial/*';
+const destination = 'View/dist';
 const cssDest     = 'View/Style';
 const pathDocs    = ['./docs/docco/', './docs/jsdoc/'];
+
+const optionsMinHTML = {
+  collapseWhitespace: true,
+  removeCommentsFromCDATA: true,
+  removeComments: true,
+  collapseBooleanAttributes: true,
+  removeEmptyAttributes: true
+};
 
 // Tâche "watch"
 gulp.task('watch', () => {
@@ -53,17 +65,33 @@ gulp.task('styles', function(){
     .pipe(gulp.dest(cssDest));
 });
 
-gulp.task('prod', () => {
+gulp.task('image', function () {
+  gulp.src(imageSource)
+    .pipe(image())
+    .pipe(gulp.dest(`${destination}/Img`));
+});
+
+gulp.task('html', function() {
   return gulp.src(htmlSource)
+    .pipe(htmlmin(optionsMinHTML))
+    .pipe(gulp.dest(`${destination}/Partial`));
+});
+
+gulp.task('prod', () => {
+  return gulp.src(indexSource)
     .pipe(plugins.plumber())
     .pipe(plugins.useref())
     .pipe(gulpif('*.js', uglify()))
-    .pipe(gulpif('*.css', inifyCss()))
+    .pipe(gulpif('*.css', minifyCss({debug: false}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    })))
+    .pipe(gulpif('*.html', htmlmin(optionsMinHTML)))
     .pipe(gulp.dest(destination));
 });
 
 gulp.task('build', (callback) => {
-  runSequence('clean', 'styles', 'prod', callback);
+  runSequence('clean', 'styles', 'html', 'image', 'prod', callback);
 });
 
 gulp.task('docs', ['docco', 'jsdoc'], () => {
--- server.js
+++ server.js
@@ -7,14 +7,14 @@
  * <b>~5 306 442</b> de lignes de code <br />
  * <br />
  * Date de Création 30/04/2016 <br />
- * Date de modification 29/04/2017 <br />
+ * Date de modification 02/09/2017 <br />
  * 
  * @version Alpha 1.4.0
  * 
  * @author Jérémy Young            <darkterra01@gmail.com>
  * @author Loïc Tardivel-Lacombe   <loic.tardivel@gmail.com>
  * @author Laura Auboin Maurizio   <lala@gaming-gen.fr>
- * @author Frédéric Guazzini       <lala@gaming-gen.fr>
+ * @author Frédéric Guazzini       <dolz@gaming-gen.fr>
  * 
  */
 
