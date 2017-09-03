82d95d080495b217aa7485b8926898eea3b8c649
darkterra <darkterra01@gmail.com>
2017-09-03 12:52:16 +0000

    Optimisation des images

--- View/ServiceWorker/main.js
+++ View/ServiceWorker/main.js
@@ -2,6 +2,10 @@
 
 const CACHE_NAME = 'gaming-gen-cache-v1';
 let urlsToCache = [
+  '/',
+  '/index.html',
+  '/please-wait.min.js',
+  '/Javascript/app.js',
   '../Img/Slider/SLIDER-GG6V4.png',
   '../Img/Partenaires/SILVER-RUSH.jpg',
   // 'https://static.hotjar.com/c/hotjar-390971.js?sv=5',
@@ -10,7 +14,7 @@ let urlsToCache = [
   // 'https://www.google-analytics.com/analytics.js'
 ];
 
-this.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       console.log('Opened cache');
@@ -23,9 +27,65 @@ this.addEventListener('install', function(event) {
   );
 });
 
-this.addEventListener('fetch', function(event) {
+// self.addEventListener('fetch', function(event) {
+//   console.log('try to fetch:' + event.request);
+//   event.respondWith(
+//     caches.match(event.request)
+//   );
+// });
+
+
+self.addEventListener('fetch', function(event) {
   console.log('try to fetch:' + event.request);
-  event.respondWith(
-    caches.match(event.request)
-  );
+  const requestURL = new URL(event.request.url);
+  
+  if (/^(\/css\/|\/js\/)/.test(requestURL.pathname)) {
+    event.respondWith(returnFromCacheOrFetch(event.request, CACHE_NAME));
+  }
+  else if (/^\/images.*\.(jpg|png)$/.test(requestURL.pathname)) {
+    event.respondWith(returnWebpOrOriginal(event.request));
+  }
 });
+
+// this.addEventListener('foreignfetch', event => {
+//   console.log('try to foreignfetch:' + event.request);
+// 	event.respondWith(fetch(event.request).then(response => {
+// 		return {
+// 			response: response,
+// 			origin: event.origin,
+// 			headers: ['Content-Type']
+// 		};
+// 	}));
+// });
+
+function returnFromCacheOrFetch(request) {
+  return caches.open(CACHE_NAME).then(function(cache) {
+    return cache.match(event.request).then(function(cacheResponse) {
+      return cacheResponse || fetch(request);
+    });
+  });
+}
+
+
+function returnWebpOrOriginal(request) {
+  // Start by assuming the browser doesn't support webp
+  let supportsWebp = false;
+
+  // If the request's Accept header contains "webp" then we can assume the browser will happily deal with a webp file.
+  if (request.headers.has('accept')) {
+    supportsWebp = request.headers.get('accept').includes('webp');
+  }
+
+  if (supportsWebp) {
+    // If we support webp then adjust the URL to ask for the webp file
+    const webpUrl = request.url.replace(/(jpg|png)$/, "webp");
+    // Then use fetch to return the webp file
+    return fetch(webpUrl).then(function(response) {
+      // If not all the images have been converted then we fallback to requesting the original file.
+      return response.status === 404 ? fetch(request) : response;
+    });
+  } else {
+    // If the browser doesn't support webp, just fetch the original request.
+    return fetch(request);
+  }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,32 @@
 const gulp        = require('gulp');
 const jsdoc       = require('gulp-jsdoc3');
 const gulpif      = require('gulp-if');
+const gulpIgnore  = require('gulp-ignore');
 const htmlmin     = require('gulp-htmlmin');
 const uglify      = require('gulp-uglify');
 const minifyCss   = require('gulp-clean-css');
 const sass        = require('gulp-sass');
 const image       = require('gulp-image');
+const imageResize = require('gulp-image-resize');
+const webp        = require('gulp-webp');
+const rename      = require("gulp-rename");
 const runSequence = require('run-sequence');
 
 // Include plugins
 const plugins = require('gulp-load-plugins')(); // tous les plugins de package.json
 
 // Variables de chemins
-const sources     = ['server.js', 'Controller/*.js', 'Model/*.js', 'View/Javascript/Controllers/*.js', 'View/Javascript/Factories/*.js', 'View/Javascript/Tests/*.js'];
-const sassFiles   = ['View/Style/*.+(scss|sass)'];
-const imageSource = 'View/Img/**';
-const indexSource = 'View/index.html';
-const htmlSource  = 'View/Partial/**';
-const destination = 'View/dist';
-const cssDest     = 'View/Style';
-const pathDocs    = ['./docs/docco/', './docs/jsdoc/'];
+const sources            = ['server.js', 'Controller/*.js', 'Model/*.js', 'View/Javascript/Controllers/*.js', 'View/Javascript/Factories/*.js', 'View/Javascript/Tests/*.js'];
+const sassFiles          = ['View/Style/*.+(scss|sass)'];
+const imageSource        = 'View/Img/**';
+const imageSourceResize  = 'View/dist/Img/**';
+const indexSource        = 'View/index.html';
+const htmlSource         = 'View/Partial/**';
+const destination        = 'View/dist';
+const cssDest            = 'View/Style';
+const pathDocs           = ['./docs/docco/', './docs/jsdoc/'];
+
+const ImageResizeMobile = 300;
 
 const optionsMinHTML = {
   collapseWhitespace: true,
@@ -65,9 +72,27 @@ gulp.task('styles', function(){
     .pipe(gulp.dest(cssDest));
 });
 
-gulp.task('image', function () {
-  gulp.src(imageSource)
+gulp.task('optimizeImg', function () {
+  gulp.src(imageSourceResize, { nodir: true })
     .pipe(image())
+    .pipe(gulp.dest(`${destination}/Img`))
+    .pipe(webp({method: 6}))
+    .pipe(gulp.dest(`${destination}/Img`));
+    
+});
+
+gulp.task('resizeImg', function () {
+  gulp.src(imageSource, { nodir: true })
+    .pipe(gulp.dest(`${destination}/Img`))
+    .pipe(gulpIgnore.exclude('*.svg'))
+    .pipe(gulpIgnore.exclude('**/OPTIMIZED*'))
+    .pipe(imageResize({
+      width: ImageResizeMobile,
+      quelity: 1
+    }))
+    .pipe(rename(function (path) {
+      path.basename += `-${ImageResizeMobile}`;
+    }))
     .pipe(gulp.dest(`${destination}/Img`));
 });
 
@@ -91,7 +116,7 @@ gulp.task('prod', () => {
 });
 
 gulp.task('build', (callback) => {
-  runSequence('clean', 'styles', 'html', 'image', 'prod', callback);
+  runSequence('clean', 'styles', 'html', 'resizeImg', 'prod', callback);
 });
 
 gulp.task('docs', ['docco', 'jsdoc'], () => {
